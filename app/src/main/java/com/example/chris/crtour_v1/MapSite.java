9a4818e817d4c0cbe694803c2fe82768df326c40
Christian Moraga <christiangeo94@gmail.com>
2016-09-17 14:08:36 -0600

    cmoraga, marker en Nicoya y drawable para boommenu.

--- app/src/main/java/com/example/chris/crtour_v1/MapSite.java
+++ app/src/main/java/com/example/chris/crtour_v1/MapSite.java
@@ -1,7 +1,19 @@
 package com.example.chris.crtour_v1;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +21,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Types.BoomType;
+import com.nightonke.boommenu.Types.ButtonType;
+import com.nightonke.boommenu.Types.PlaceType;
+import com.nightonke.boommenu.Util;
+
+import java.util.Random;
+
 
 public class MapSite extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private BoomMenuButton boomMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,9 @@ public class MapSite extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+//        boomMenuButton = (BoomMenuButton) mappingView.findViewById(R.id.boom);
     }
 
 
@@ -29,8 +53,8 @@ public class MapSite extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Nicoya = new LatLng(10.140466, -85.449841);
+        mMap.addMarker(new MarkerOptions().position(Nicoya).title("Nicoya"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Nicoya));
     }
 }
